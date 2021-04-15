92c9accd5ba4a56a952a76bd2b85f05e7f4fddc9
Seif <seifallahkaak@gmail.com>
2021-04-15 09:01:22 -0400

    tester le push pull

---
+++ Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AqlProjet_Final
+{
+    class Student
+    {
+        //definition des attributs de la classe Etudiant
+
+        public int NumeroEtudiant { get; set; }
+        public String Nom { get; set; }
+        public String Prenom { get; set; }
+
+        // definition du premier constructeur
+
+        public Student()
+        {
+
+        }
+
+        // definition du second constructeur
+        public Student(int numeroEtudiant)
+        {
+            this.NumeroEtudiant = numeroEtudiant;
+        }
+
+        // definition du troixieme constructeur
+        public Student(int numeroEtudiant, String nom, String prenom) : this(numeroEtudiant)
+        {
+
+            this.Nom = nom;
+            this.Prenom = prenom;
+        }
+
+
+
+    }
+}
+
